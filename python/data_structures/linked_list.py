2a3abb5710ee7ddc6698dab3c35ad270e9128308
Christopher Harden <Harden.Christopher00@gmail.com>
2022-05-04 03:01:22 -0500

    pushing anywork before new branch

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -5,12 +5,17 @@ class LinkedList:
 
     def __init__(self):
         # initialization here
+        self.head = None
+
+    def insert(self):
+        # creating a new node w/ the correct value
+        new_node = Node("apple")
+        self.head = Node("apple")
         pass
 
-    def some_method(self):
-        # method body here
-        pass
-
-
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 class TargetError:
     pass
